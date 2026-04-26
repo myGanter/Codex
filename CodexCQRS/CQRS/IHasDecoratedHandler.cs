ac0b8ec4aa308ee9ce491390d6ce3b310fcd2e03
myGanter <estji004@gmail.com>
2026-04-26 20:33:07 +0400

    [CodexNet10] custom handler interfaces

---
+++ CodexCQRS/CQRS/IHasDecoratedHandler.cs
@@ -0,0 +1,28 @@
+﻿using CodexCQRS.Dtos;
+
+namespace CodexCQRS.CQRS
+{
+    public interface IHasDecoratedHandler<TDto>
+    {
+        public abstract IHandler<TDto> DecoratedHandler { get; set; }
+    }
+
+    public interface IHasAsyncDecoratedHandler<TDto>
+    {
+        public abstract IAsyncHandler<TDto> DecoratedAsyncHandler { get; set; }
+    }
+
+    public interface IHasDecoratedHandler<TDto, TOut, TError>
+        where TError : class
+        where TDto : IDtoContract<TOut, TError>
+    {
+        public abstract IHandler<TDto, TOut, TError> DecoratedResultHandler { get; set; }
+    }
+
+    public interface IHasAsyncDecoratedHandler<TDto, TOut, TError>
+        where TError : class
+        where TDto : IDtoContract<TOut, TError>
+    {
+        public abstract IAsyncHandler<TDto, TOut, TError> DecoratedAsyncResultHandler { get; set; }
+    }
+}
--- CodexCQRS/Dispatcher/IDispatcher.cs
+++ CodexCQRS/Dispatcher/IDispatcher.cs
@@ -1,4 +1,5 @@
-﻿using CodexCQRS.Dtos;
+﻿using CodexCQRS.CQRS;
+using CodexCQRS.Dtos;
 
 namespace CodexCQRS.Dispatcher
 {
@@ -8,9 +9,13 @@ namespace CodexCQRS.Dispatcher
 
         void Dispatch(object dto, bool buildDecorator = true);
 
+        (IHandler<TDto> SourceHandler, IHandler<TDto> DecoratedHandler) BuildHandler<TDto>();
+
         Task DispatchAsync<TDto>(TDto dto, bool buildDecorator = true, CancellationToken token = default);
 
         Task DispatchAsync(object dto, bool buildDecorator = true, CancellationToken token = default);
+
+        (IAsyncHandler<TDto> SourceHandler, IAsyncHandler<TDto> DecoratedHandler) BuildAsyncHandler<TDto>();
 
         ResultOr<TOut, TError> DispatchResult<TDto, TOut, TError>(TDto dto, bool buildDecorator = true)
             where TError : class
@@ -18,10 +23,18 @@ namespace CodexCQRS.Dispatcher
 
         object DispatchResult(object dto, bool buildDecorator = true);
 
+        (IHandler<TDto, TOut, TError> SourceHandler, IHandler<TDto, TOut, TError> DecoratedHandler) BuildResultHandler<TDto, TOut, TError>()
+            where TError : class
+            where TDto : IDtoContract<TOut, TError>;
+
         Task<ResultOr<TOut, TError>> DispatchResultAsync<TDto, TOut, TError>(TDto dto, bool buildDecorator = true, CancellationToken token = default)
             where TError : class
             where TDto : IDtoContract<TOut, TError>;
 
         Task<object> DispatchResultAsync(object dto, bool buildDecorator = true, CancellationToken token = default);
+
+        (IAsyncHandler<TDto, TOut, TError> SourceHandler, IAsyncHandler<TDto, TOut, TError> DecoratedHandler) BuildAsyncResultHandler<TDto, TOut, TError>()
+            where TError : class
+            where TDto : IDtoContract<TOut, TError>;
     }
 }
